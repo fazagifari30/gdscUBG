eaa4754d5a65c272eff3b4b5a1874207f703e0ba
fazagifari30 <fazagifari30@gmail.com>
2022-07-15 18:14:46 +0800

    memulai

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./page/**/*.html"],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#ffffff',
+        secondary: '#ecc94b',
+      }
+    },
+  },
+  plugins: [],
+}
